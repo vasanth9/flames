0b6e09e2e659518e060f1236fda9f2432a079650
vasanth9 <cheepurupalli.vasanthkumar@gmail.com>
2020-08-29 11:52:10 +0530

    Updates

---
+++ precache-manifest.b56732ea8b28452a0fca88419b014b79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b75f6186c0099cfa884fe8238ce75b3e",
+    "url": "/flames/index.html"
+  },
+  {
+    "revision": "661bd6790e2a7b5e830d",
+    "url": "/flames/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "20447fe9bdc0511ab2d7",
+    "url": "/flames/static/js/2.7675cd7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/flames/static/js/2.7675cd7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "661bd6790e2a7b5e830d",
+    "url": "/flames/static/js/main.e8d0336e.chunk.js"
+  },
+  {
+    "revision": "819cb8f3834ae044615d",
+    "url": "/flames/static/js/runtime-main.9766cf85.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/flames/static/media/logo.5d5d9eef.svg"
+  }
+]);
